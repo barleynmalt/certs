14909a205397272069a12908dbf1c9fd27bef680
Your Name <you@example.com>
2022-07-22 22:29:11 +1000

    use password when generating CA key

--- 01-generate-root-ca-key.sh
+++ 01-generate-root-ca-key.sh
@@ -7,17 +7,30 @@
 
 # Check command line arguments.
 if [ $# -lt 1 ] ; then
-   echo "Usage: $0 <algorithm> <bits: default=4096>"
-   echo "Exmaple: ./01-generate-root-ca-key.sh des3 [4096]"
+   echo "Usage: $0 <algorithm> <bits: default=4096> <use-passprase: y/n>"
+   echo "Exmaple: ./01-generate-root-ca-key.sh des3 [4096] [y]"
    exit 1
 fi
 
 # Encryption algorithm
-ENCRYPTION_ALGO=$1
+ENCRYPTION_ALGO=`echo ${1-rsa} | tr [:lower:] [:upper:]`
+echo "Encryption algorithm: ${ENCRYPTION_ALGO}"
+if [ "$ENCRYPTION_ALGO" == "RSA" ] || [ "$ENCRYPTION_ALGO" == "DES3" ] ; then
+   ENCRYPTION_ALGO=$ENCRYPTION_ALGO
+else
+   echo "Unsupported encryption algorithm"
+   exit
+fi
 
 # Key length. If not provided in the command line argument
 # 4096 is used as the default.
 KEY_SIZE_IN_BITS=${2-4096}
+
+# Use passphrase
+USE_PASS=`echo ${3-Y} | tr [:lower:] [:upper:]`
+if [ "$USE_PASS" != "Y" ] ; then
+   USE_PASS=N
+fi
 
 PRIVATE_KEY_OUTFILE="root/root-ca-private.key"
 
@@ -29,4 +42,8 @@ if [ -e $PRIVATE_KEY_OUTFILE ] ; then
 fi
 
 echo "Generating Root CA private key for ${PRIVATE_KEY_OUTFILE} ..."
-openssl genrsa -out $PRIVATE_KEY_OUTFILE $KEY_SIZE_IN_BITS
+if [ "$USE_PASS" == "Y" ] ; then
+   openssl genrsa -aes256 -out $PRIVATE_KEY_OUTFILE $KEY_SIZE_IN_BITS
+else
+   openssl genrsa -out $PRIVATE_KEY_OUTFILE $KEY_SIZE_IN_BITS
+fi
